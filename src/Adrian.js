470f666892b62e4dc3491827039f3c59c26b95cf
206 <206@academicos.cesde.edu.co>
2022-06-01 09:12:45 -0500

    se crea cliente

---
+++ src/Adrian.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react"
+
+export function Adrian(){
+
+    let universidad='CESDE'
+    let suma=5+1
+
+    return(
+        <>
+            <h1>hola soy tu Adrian</h1>
+            {/*<h4>y su amigo...</h4>*/}
+            <h5>y estudio en {universidad}</h5>
+            <h6>la suma de 5+1 es {5+1}</h6>
+        </>
+       
+    )
+}
